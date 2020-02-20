8df7f13be9ec06c1e92dd3aabaaea41ff8fdaeaa
Stuart <robinjamesauthor@gmail.com>
2020-02-20 14:17:28 +0200

    [WIP] seperating vuex into namespaced modules

--- src/store/index.js
+++ src/store/index.js
@@ -7,33 +7,12 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   //State is equivalent to data
   state: {
-    products: [],
-    // to store id and quantity
-    cart: [],
-    checkoutStatus: null
+    products: []
   },
   //getters are equivalents to computed properties
   getters: {
     availableProducts(state, getters) {
       return state.products.filter(product => product.inventory > 0)
-    },
-    cartProducts(state) {
-      return state.cart.map(cartItem => {
-        const product = state.products.find(product => product.id === cartItem.id)
-        return {
-          title: product.title,
-          price: product.price,
-          quantity: cartItem.quantity
-        }
-      })
-    },
-    cartTotal(state, getters) {
-      // let total = 0
-      // getters.cartProducts.forEach(product => {
-      //   total += product.price * product.quantity
-      // })
-      // return total
-      return getters.cartProducts.reduce((total, product) => total + product.price * product.quantity, 0)
     },
     productIsInStock() {
       return (product) => {
@@ -84,23 +63,8 @@ export default new Vuex.Store({
     setProducts(state, products) {
       state.products = products
     },
-    pushProductToCart(state, productId) {
-      state.cart.push({
-        id: productId,
-        quantity: 1
-      })
-    },
-    incrementItemQuantity(state, cartItem) {
-      cartItem.quantity++
-    },
     decrementProductInventory(state, product) {
       product.inventory--
-    },
-    setCheckoutStatus(state, status) {
-      state.checkoutStatus = status
-    },
-    emptyCart(state) {
-      state.cart = []
     }
   }
 })
